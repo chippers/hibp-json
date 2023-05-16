57f6c38da25749d641520ac5726ea0e9610f075e
Chip Reed <chip@chip.sh>
2023-05-16 03:08:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# hibp-json
+
+Turns HaveIBeenPwned (HIBP) passsword hash files into JSON format, along with compressing them (optionally) into `.gz` and/or `.br` files for quick static serving.
+
+## Prerequisites
+
+You must have aquired all of the HIBP hash files through the [PwnedPasswordsDownloader](https://github.com/HaveIBeenPwned/PwnedPasswordsDownloader) using the individual hash files option. This is their example of how to use their downloader that downloads into individual hash files (`-s false`) with 64 threads (`-p 64`). This step will take a while, and you should take care not to delete the created directory otherwise you will have to re-download everything.
+
+```
+haveibeenpwned-downloader.exe hashes -s false -p 64
+```
+
+## Running
+
+By default `hibp-json` expects the hashes to be in `hashes/` and the output to be created in `dist/`. This is configurable, see `hibp-json --help`.
+
+By default, `.json` files, `.json.gz` files, and `.json.br` files will be created. Each of these can be turned off, see `hibp-json --help`.
+
+## Size
+
+Here are the size of the raw files. Notably, the original format is about as efficient as possible (the first 5 chars of the hash being excluded because its in the filename) and each line is just `{hash}:{count}`. Because of this, the JSON size is somewhat larger because the full hash is included to prevent needing to remember to concat the hashes on the frontend alongside each item becoming a JSON object with `hash` and `count` fields.
+
+This is not a huge deal, because the compression algorithms are able to cut down on the repetitive structure of the JSON, bringing the compressed versions to the size of the original version when compressed. e.g. `000D0.txt` is `35,905` bytes raw and `17,190` bytes compressed with brotli while `000D0.json` is `57,045` bytes raw and `17,405` bytes compressed.
+
+Thus, if you decide to **only** serve compressed assets, then you don't need the raw JSON files and can exclude them from generation by adding `--json false`. This allows you to only serve gzip or brotli compressed files, and possibly optionally decompressing on-the-fly when requested for a non-compressed file. Virtually every browser and most tools support gzip compressed content, and a [very large amount (96.6%) of browsers support brotli](https://caniuse.com/brotli).
+
+| Source | Size | Time |
+| ------ | ---- | ---- |
+| Original | ~`33 GiB` | n/a |
+| JSON | ~`50 GiB` | `3m` |
+| Gzip | ~`17.3 GiB` | `5m` |
+| Brotli | ~`16 GiB` | `90m` |
+
+<sup>note: timings taken on my Macbook Pro m2 Pro</sup>
+
+I suggest only choosing a single compressed version based on your requirements and excluding generating raw JSON files. Brotli has slightly lower global support (but still extremely high) and takes consideribly longer to generate but generates 10% smaller files.
+
+## Examples
+
+This show generating only json, json + gz, json + br, and json + gz + br in order.
+
+```console
+chip@cancer hibp-json % rm -rf dist && /usr/bin/time ./target/release/hibp-json --strict false --brotli false --gzip false
+[1/3] Ensured 65,536 output directories in 1709ms
+[2/3] Found 2048 hash files in benchdata in 1ms
+[3/3] Generating .json files 
+Finished generating files in 68ms (1778ms total)
+Bytes: json 105166422 | br 0 | gz 0
+        1.78 real         0.22 user         1.87 sys
+
+chip@cancer hibp-json % rm -rf dist && /usr/bin/time ./target/release/hibp-json --strict false --brotli false             
+[1/3] Ensured 65,536 output directories in 1626ms
+[2/3] Found 2048 hash files in benchdata in 2ms
+[3/3] Generating .json .gz files 
+Finished generating files in 354ms (1983ms total)
+Bytes: json 105166422 | br 0 | gz 36263187
+        1.98 real         2.80 user         2.08 sys
+
+chip@cancer hibp-json % rm -rf dist && /usr/bin/time ./target/release/hibp-json --strict false --gzip false 
+[1/3] Ensured 65,536 output directories in 1644ms
+[2/3] Found 2048 hash files in benchdata in 4ms
+[3/3] Generating .json .br files 
+Finished generating files in 10536ms (12186ms total)
+Bytes: json 105166422 | br 32191367 | gz 0
+       12.19 real       112.09 user         2.70 sys
+
+chip@cancer hibp-json % rm -rf dist && /usr/bin/time ./target/release/hibp-json --strict false               
+[1/3] Ensured 65,536 output directories in 1758ms
+[2/3] Found 2048 hash files in benchdata in 4ms
+[3/3] Generating .json .br .gz files 
+Finished generating files in 10817ms (12580ms total)
+Bytes: json 105166422 | br 32191367 | gz 36263187
+       12.59 real       114.42 user         3.05 sys
+```
---
+++ src/main.rs
@@ -0,0 +1,265 @@
+use std::{
+    ffi::OsStr,
+    fs::File,
+    io::{stdout, BufRead, BufWriter, Write},
+    path::{Path, PathBuf},
+    sync::atomic::{AtomicU64, Ordering},
+    time::Instant,
+};
+
+use anyhow::Result;
+use clap::{ArgAction, Parser};
+use console::style;
+use flate2::Compression;
+use indicatif::{ParallelProgressIterator, ProgressStyle};
+use mimalloc::MiMalloc;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+use serde::Serialize;
+use walkdir::WalkDir;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+/// Generate JSON formatted files for HIBP password hash files
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[allow(clippy::struct_excessive_bools)]
+struct Args {
+    /// Path to existing hashes
+    #[arg(long, default_value = "hashes")]
+    hashes: PathBuf,
+
+    /// Path to output to
+    #[arg(short, long, default_value = "dist")]
+    out: PathBuf,
+
+    /// If the input should be strictly checked
+    #[arg(long, default_value_t = true, action = ArgAction::Set)]
+    strict: bool,
+
+    /// If .gz files should be generated
+    #[arg(long, default_value_t = true, action = ArgAction::Set)]
+    gzip: bool,
+
+    /// If .br files should be generated
+    #[arg(long, default_value_t = true, action = ArgAction::Set)]
+    brotli: bool,
+
+    /// If .json files should be generated
+    #[arg(long, default_value_t = true, action = ArgAction::Set)]
+    json: bool,
+}
+
+#[derive(Serialize)]
+struct Password {
+    hash: String,
+    count: usize,
+}
+
+fn walk1(path: impl AsRef<Path>) -> walkdir::IntoIter {
+    WalkDir::new(path.as_ref())
+        .max_depth(1)
+        .min_depth(1)
+        .into_iter()
+}
+
+fn generate_out_structure(out: &Path) -> Result<()> {
+    #[rustfmt::skip]
+    let hex = [
+        "0", "1", "2", "3", "4", "5", "6", "7",
+        "8", "9", "A", "B", "C", "D", "E", "F",
+    ];
+
+    for c1 in hex {
+        let p1 = out.join(c1);
+        for c2 in hex {
+            let p2 = p1.join(c2);
+            for c3 in hex {
+                let p3 = p2.join(c3);
+                for c4 in hex {
+                    std::fs::create_dir_all(p3.join(c4))?;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn flush() -> Result<()> {
+    Ok(stdout().lock().flush()?)
+}
+
+fn ensure_output_directories(dist: &Path) -> Result<()> {
+    print!(
+        "{} Ensuring 65,536 output directories",
+        style("[1/3]").bold().dim()
+    );
+    flush()?;
+
+    let start = Instant::now();
+    generate_out_structure(dist)?;
+
+    println!(
+        "\r{} Ensured 65,536 output directories in {}ms",
+        style("[1/3]").bold().dim(),
+        style(start.elapsed().as_millis()).bold()
+    );
+
+    Ok(())
+}
+
+fn find_all_hash_files(hashes: &Path) -> Result<Vec<PathBuf>> {
+    print!(
+        "{} Finding all hash files in {}",
+        style("[2/3]").bold().dim(),
+        style(hashes.display()).bold()
+    );
+    flush()?;
+
+    let start = Instant::now();
+    let paths =
+        walk1(hashes).try_fold(Vec::with_capacity(1_048_576), |mut acc: Vec<_>, item| {
+            acc.push(item?.into_path());
+            Ok::<_, walkdir::Error>(acc)
+        })?;
+
+    println!(
+        "\r{} Found {} hash files in {} in {}ms",
+        style("[2/3]").bold().dim(),
+        style(paths.len()).bold(),
+        style(hashes.display()).bold(),
+        style(start.elapsed().as_millis()).bold(),
+    );
+
+    Ok(paths)
+}
+
+fn progress_style() -> ProgressStyle {
+    ProgressStyle::with_template("{elapsed} {bar} {percent}% eta {eta} {per_sec} ")
+        .unwrap()
+        .progress_chars("█▉▊▋▌▍▎▏  ")
+}
+
+fn format_prefix_to_dirs(prefix: &str) -> String {
+    prefix
+        .char_indices()
+        .fold(String::with_capacity(9), |mut acc, (i, c)| {
+            if i > 0 {
+                acc.push('/');
+            }
+            acc.push(c);
+            acc
+        })
+}
+
+fn main() -> Result<()> {
+    let very_start = Instant::now();
+    let args = Args::parse();
+
+    ensure_output_directories(&args.out)?;
+    let paths = find_all_hash_files(&args.hashes)?;
+    let count = paths.len() as u64;
+
+    if args.strict {
+        // HIBP has every single 5 character prefix of sha1
+        assert!(count == 16_u64.pow(5));
+    }
+
+    let (json, brotli, gzip) = (args.json, args.brotli, args.gzip);
+
+    println!(
+        "{} Generating{}{}{} files ",
+        style("[3/3]").bold().dim(),
+        if json { " .json" } else { "" },
+        if brotli { " .br" } else { "" },
+        if gzip { " .gz" } else { "" }
+    );
+
+    let dist = args.out.as_path();
+
+    let start = Instant::now();
+
+    let total_json = AtomicU64::new(0);
+    let total_gz = AtomicU64::new(0);
+    let total_br = AtomicU64::new(0);
+
+    paths
+        .into_par_iter()
+        .progress_with_style(progress_style())
+        .for_each(|path| {
+            let prefix = path.file_stem().and_then(OsStr::to_str).unwrap();
+            let mut passwords = Vec::with_capacity(2048);
+
+            let content = std::fs::read(&path).unwrap();
+            for line in content.lines().map(Result::unwrap) {
+                let mut hash = String::with_capacity(40);
+                let (h, c) = line.split_once(':').unwrap();
+                let count = c.parse().unwrap();
+                hash.push_str(prefix);
+                hash.push_str(h);
+                passwords.push(Password { hash, count });
+            }
+
+            let prefix = format_prefix_to_dirs(prefix);
+
+            let serialized = serde_json::to_vec(&passwords).unwrap();
+
+            if json {
+                let content = serialized.as_slice();
+                std::fs::write(dist.join(format!("{prefix}.json")), content).unwrap();
+                total_json.fetch_add(content.len() as u64, Ordering::SeqCst);
+            }
+
+            /*if zopfli {
+                let file = File::create(dist.join(format!("{prefix}.json.gz"))).unwrap();
+                let mut buf: BufWriter<File> = BufWriter::new(file);
+                zopfli::compress(
+                    &Default::default(),
+                    &zopfli::Format::Gzip,
+                    serialized.as_slice(),
+                    &mut buf,
+                )
+                .unwrap();
+            }*/
+            if gzip {
+                let file = File::create(dist.join(format!("{prefix}.json.gz"))).unwrap();
+                let mut buf: BufWriter<File> = BufWriter::new(file);
+                let mut enc = flate2::write::GzEncoder::new(&mut buf, Compression::best());
+                enc.write_all(&serialized).unwrap();
+                enc.finish().unwrap();
+
+                let f = buf.into_inner().unwrap();
+                let size = f.metadata().unwrap().len();
+                total_gz.fetch_add(size, Ordering::SeqCst);
+            }
+
+            if brotli {
+                let mut serialized = std::io::Cursor::new(serialized);
+                let file = File::create(dist.join(format!("{prefix}.json.br"))).unwrap();
+                let mut buf: BufWriter<File> = BufWriter::new(file);
+                let size = brotli::BrotliCompress(
+                    &mut serialized,
+                    &mut buf,
+                    &brotli::enc::BrotliEncoderInitParams(),
+                )
+                .unwrap();
+                total_br.fetch_add(size as u64, Ordering::SeqCst);
+            }
+        });
+
+    println!(
+        "Finished generating files in {}ms ({}ms total)",
+        style(start.elapsed().as_millis()).bold(),
+        very_start.elapsed().as_millis()
+    );
+
+    println!(
+        "Bytes: json {} | br {} | gz {}",
+        total_json.into_inner(),
+        total_br.into_inner(),
+        total_gz.into_inner()
+    );
+
+    Ok(())
+}
